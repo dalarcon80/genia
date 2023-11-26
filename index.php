1266b97631d7c2566a9bd4eb6b8d236b380ddfa2
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2023-11-26 12:27:57 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Embedded Script</title>
+</head>
+<body>
+
+<!-- Aquí es donde se embeberá el script -->
+<div id="soulmachines-widget"></div>
+
+<!-- El script embebido -->
+<script src="https://static.soulmachines.com/widget-snippet-1.12.0.min.js"
+    data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWRhbmllbC1hbGFyY29uLS1jZW5pdC1hY2NlbnR1cmUtYXZhdGFyIiwiYXV0aFNlcnZlciI6Imh0dHBzOi8vZGguYXouc291bG1hY2hpbmVzLmNsb3VkL2FwaS9qd3QiLCJhdXRoVG9rZW4iOiJhcGlrZXlfdjFfNThiOTE0ZmQtNzhlNy00MDY0LWFlZmQtOWEzNDRiNzFmOGI1In0="
+></script>
+
+</body>
+</html>
